7b443774659fecb8f9379d5b3034b37066ba659e
Pedro Perez <pipr96@gmail.com>
2023-07-31 19:36:12 -0700

    add react-router-dom

---
+++ react-route/src/components/Nav.jsx
@@ -0,0 +1,12 @@
+//This is the navigation tab
+import Link from 'react-router-dom'
+
+
+export default function Nav(){
+
+    return(
+        <nav>
+           <Link to={"/"}/>
+        </nav>
+    )
+}
