0544f426de115e3b49703348632367ddd7da9044
Pedro Perez <pipr96@gmail.com>
2023-08-03 11:12:01 -0700

    Add home components

---
+++ react-route/src/components/Home.jsx
@@ -0,0 +1,17 @@
+
+
+export default function Home(){
+
+    return(
+        <div id='main-content'>
+            <h2>React Router Tutorial</h2>
+            <ol>
+                <h3>Components</h3>
+                <li>BrowserRouter</li>
+                <li>Router</li>
+                <li>Routes</li>
+                <li>Route</li>
+            </ol>
+        </div>
+    )
+}
