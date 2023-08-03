c79d43cf967273588b9013275f531808e5bcd9a5
Pedro Perez <pipr96@gmail.com>
2023-08-03 11:09:38 -0700

    add Links

--- react-route/src/components/Footer.jsx
+++ react-route/src/components/Footer.jsx
@@ -1,11 +1,14 @@
-
-
+import {Link} from 'react-router-dom'
 export default function Footer(){
 
     return(
         <footer className="container">
-            <article>
+            <article >
                 <h4>Colors</h4>
+                <ol>
+                <li><Link to={'/colors/red'}>Red</Link></li>
+                <li><Link to={'/colors/blue'}>Blue</Link> </li>
+                </ol>
             </article>
             <article>
                 <h4>About us</h4>
